84bc5fa85fca67a6781e202e0745ffb25dec3c98
GiPhantom <f4ringeityandex.ru>
2021-04-13 10:10:33 +0000

    Initial commit

--- src/index.js
+++ src/index.js
@@ -15,4 +15,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-//https://www.youtube.com/watch?v=EeCmCB3tsas&list=PLVfMKQXDAhGWamOcqqIsMAbsP6pl0vUtW&index=3
