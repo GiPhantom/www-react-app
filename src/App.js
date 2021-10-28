d076e2ed236eb41bb58e98ea1e7ffc7a44dee901
GiPhantom <f4ringeit@yandex.ru>
2021-10-28 09:03:40 +0000

    INIT App.js

--- src/App.js
+++ src/App.js
@@ -1,69 +1,23 @@
-import React, { useState } from "react";
-import "./App.css";
-import Item from "./components/Item";
-import { v4 as uuidv4 } from "uuid";
-
-const arr = () => {
-  let data = localStorage.getItem("data");
-  if (data) return JSON.parse(localStorage.getItem("data"));
-  else return [];
-};
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [item, setItem] = useState("");
-  const [list, setList] = useState(arr);
-
-  const handleSubmit = (e) => {
-    const newItem = {
-      id: uuidv4(),
-      item: item,
-      complete: false,
-    };
-    e.preventDefault();
-    if (item && item.length <= 25) {
-      setList([...list, newItem]);
-      setItem("");
-    }
-  };
-
-  React.useEffect(() => {
-    localStorage.setItem("data", JSON.stringify(list));
-  }, [list]);
-
-  const handleChange = (e) => {
-    setItem(e.target.value);
-  };
-
   return (
     <div className="App">
-      <h1>Grocery List</h1>
-            <form onSubmit={handleSubmit}>
-        <input
-          className="input"
-          type="text"
-          value={item}
-          placeholder="Enter the items"
-          onChange={handleChange}
-        />
-        <button className="btn" type="submit">
-          Add Items
-        </button>
-        <br></br>
-        <br></br>
-      </form>
-      <div>
-        {list.map((c, id) => (
-          <Item
-            key={id}
-            id={c.id}
-            item={c.item}
-            list={list}
-            setList={setList}
-            complete={c.complete}
-            setItem={setItem}
-          />
-        ))}
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
